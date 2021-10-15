1017a140239d5325eed89fbccdead6a7cad8fd4a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-10-15 14:30:40 +0300

    Add Username Login And Register

---
+++ 01.RoomScheduler.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RoomScheduler.Data;
+using RoomScheduler.Data.Models;
+
+[assembly: HostingStartup(typeof(RoomScheduler.Web.Areas.Identity.IdentityHostingStartup))]
+namespace RoomScheduler.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ 01.RoomScheduler.Web/Extensions/IdentityExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RoomScheduler.Data.Models;
+using System.Threading.Tasks;
+
+namespace RoomScheduler.Web.Extensions
+{
+    public static class IdentityExtension
+    {
+        public static async Task<string> GetSignInMethod(string emailOrUsername, UserManager<ApplicationUser> _userManager)
+        {
+            var signInMethod = emailOrUsername;
+            if (emailOrUsername.Contains('@'))
+            {
+                var user = await _userManager.FindByEmailAsync(signInMethod);
+                if (user != null)
+                {
+                    signInMethod = user.UserName;
+                }
+            }
+            return signInMethod;
+        }
+    }
+}
